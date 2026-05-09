8f41489a6846a30cf3821d239a797ee7f34b62cc
aaradhyarahi121-oss <aaradhyarahi121@gmail.com>
2026-05-09 06:50:00 +0100

    Add files via upload

---
+++ Daily routine.py
@@ -0,0 +1,18 @@
+print("My Daily Routine")
+# Step 1: Waking up
+print("I wake up at 7 AM.")
+
+# Step 2: Going to school
+print("I go to school at 8 AM.")
+
+# Step 3: Studying
+print("I revise what was taught in the lesson and complete my homework.")
+
+# Step 4: Playing after school
+print("I play with my friends in the evening.")
+
+# Step 5: tution
+print("I study for 2 hours at tution.")
+
+# Step 6: Eat
+print("I have dinner at 9pm.")
